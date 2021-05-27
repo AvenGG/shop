843310cf1f9c28a50005757cbd951abb8bd12808
Ivan Krisko <vanya9.9@mail.ru>
2021-05-27 17:57:07 +1000

    added empty item filters component

---
+++ src/components/ItemFilters.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="filters">
+       Фильтры:
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .filters{
+        width:200px;
+        height: 100%;
+        background-color: white;
+        box-shadow: 0px 0px 5px 0px rgb(116, 116, 116);
+        border-radius: 10px;
+        padding: 10px;
+        box-sizing: border-box;
+    }
+</style>
