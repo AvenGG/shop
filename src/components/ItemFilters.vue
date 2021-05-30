aa26d0bc766ee092baed5510ea89de110d9d97ad
Ivan Krisko <vanya9.9@mail.ru>
2021-05-31 00:24:38 +1000

    added filters drop down

--- src/components/ItemFilters.vue
+++ src/components/ItemFilters.vue
@@ -1,20 +1,34 @@
 <template>
     <div class="filters">
-       Фильтры:
+      <div class="filters__title">Фильтры</div>
         <div class="filter"
             v-for="filter of Object.keys(filters)"
             :key="filter"
         >
-           {{filters[filter].title}}
-           <div
-            v-for="fv of filters[filter].values"
-            :key="fv"
-           >
-           <input type="checkbox"
-            :value="fv"
-            v-model="selectedFilters[filter]"
-            @change="emitFilters()"
-           >{{fv}}
+            <button class="filter_element-button"
+                @click="showFilters[filter]  = !showFilters[filter]"
+            >
+                <div class="drop-down-arrow" :class="{open: showFilters[filter]}">
+                    <span class="drop-down-arrow-left"></span>
+                    <span class="drop-down-arrow-right"></span>
+                </div>
+                <div class="filter__title"
+                    :class="{filter__title_bold:showFilters[filter]}"
+                >
+                    {{filters[filter].title}}
+                </div>
+           </button>
+           <div v-if="showFilters[filter]">
+                <div class="filter__variants"
+                    v-for="fv of filters[filter].values"
+                    :key="fv"
+                >
+                    <input type="checkbox"
+                        :value="fv"
+                        v-model="selectedFilters[filter]"
+                        @change="emitFilters()"
+                    >{{fv}}
+                </div>
             </div>
        </div>
     </div>
@@ -24,7 +38,8 @@
 export default {
     data(){
         return{
-            selectedFilters: {}
+            selectedFilters: {},
+            showFilters:{}
         }
     },
     props:{
@@ -69,16 +84,26 @@ export default {
                 sf[f] = []
             }
             this.selectedFilters = sf
+        },
+        initShowFilters(){
+            let sf = {}
+            let categories = Object.keys(this.filters)
+
+            categories.forEach(f =>{
+                sf[f] = false;
+            })
+            this.showFilters = sf
         }
     },
     computed:{
         filters(){
             return this.setFilters()
-        },
+        }
     },
     watch:{
         items(){
             this.initSelectedFilters()
+            this.initShowFilters()
             this.$emit('filter-items', {})
         }
     }
@@ -87,11 +112,109 @@ export default {
 
 <style scoped>
     .filters{
-        width:200px;
+        width:220px;
         background-color: white;
         box-shadow: 0px 0px 5px 0px rgb(116, 116, 116);
         border-radius: 10px;
-        padding: 10px;
         box-sizing: border-box;
+        padding-bottom: 20px;
+    }
+    .filters__title{
+        text-align: center;
+        font-size: 18px;
+        padding: 10px 0;
+    }
+    .filter{
+        border: 1px solid rgb(223, 223, 223);
+        border-left: none;
+        border-right: none;
+        border-bottom: none;
+    }
+    .filter_element-button{
+        display: flex;
+        min-height: 40px;
+        align-items: center;
+        width: 100%;
+        border:none;
+        padding: 0 5px;
+        background-color: white;
+        text-align: left;
+    }
+    .filter_element-button:hover{
+        background-color: rgb(255, 230, 89);
+        
+    }
+    .filter__title{
+        font-size: 15px;
+        margin: 5px 5px;
+        
+    }
+    .filter__title_bold{
+        font-weight: bold;
+    }
+    .filter__variants{
+        font-size: 15px;
+        margin: 3px 10px;
+    }
+    
+
+    .drop-down-arrow {
+        position: relative;
+        cursor: pointer;
+        margin:0 15px 0 5px;
+        width: 2px;
+        height: 2px;
+    }
+    .drop-down-arrow-left {
+        position: absolute;
+        background-color: transparent;
+        top: 1px;
+        left: 0;
+        width: 8px;
+        height: 2px;
+        display: block;
+        transform: rotate(35deg);
+        float: right;
+    }
+    .drop-down-arrow-left:after {
+        content: "";
+        background-color: #db2ec4;
+        width: 8px;
+        height: 2px;
+        display: block;
+        float: right;
+        border-radius: 6px 10px 10px 6px;
+        transition: all 0.5s cubic-bezier(0.25, 1.7, 0.35, 0.8);
+        z-index: -1;
+    }
+    
+    .drop-down-arrow-right {
+        position: absolute;
+        background-color: transparent;
+        top: 1px;
+        left: 6px;
+        width: 8px;
+        height: 2px;
+        display: block;
+        transform: rotate(-35deg);
+        float: right;
+    }
+    .drop-down-arrow-right:after {
+        content: "";
+        background-color: #db2ec4;
+        width: 8px;
+        height: 2px;
+        display: block;
+        float: right;
+        transition: all 0.5s cubic-bezier(0.25, 1.7, 0.35, 0.8);
+        z-index: -1;
+    }
+    .open .drop-down-arrow-left:after {
+        transform-origin: center center;
+        transform: rotate(-70deg);
+    }
+    .open .drop-down-arrow-right:after {
+        transform-origin: center center;
+        transform: rotate(70deg);
     }
 </style>
